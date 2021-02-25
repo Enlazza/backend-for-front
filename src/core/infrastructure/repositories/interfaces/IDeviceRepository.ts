e17fa2e0033f3a97110532e9329cb1bcebf2a379
VadokDev <gonzalo.fdez06@gmail.com>
2021-02-25 15:42:36 -0300

    fix eexample

--- src/core/infrastructure/repositories/interfaces/IDeviceRepository.ts
+++ src/core/infrastructure/repositories/interfaces/IDeviceRepository.ts
@@ -3,5 +3,4 @@ import IDevice from '../../../entities/interfaces/IDevice';
 export default interface IDeviceRepository {
   getAll(): Promise<Array<IDevice> | null>;
   getById(id: number): Promise<IDevice | null>;
-  getByMac(mac: string): Promise<IDevice | null>;
 }
